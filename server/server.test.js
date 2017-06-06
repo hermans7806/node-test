70a48faa4e5e34995636d805b340ce154c2cd166
Herman <herman.sutanto@reebonz.com>
2017-06-06 08:24:52 +0800

    Initial commit

---
+++ server/server.test.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+const expect = require('expect');
+
+var app = require('./server').app;
+
+it('should return hello world response', (done) => {
+  request(app)
+    .get('/')
+    .expect(404)
+    .expect((res) => {
+      expect(res.body).toInclude({
+        error: 'Page not found'
+      });
+    })
+    .end(done);
+})
+
+it('should have Herman in users array', (done) => {
+  request(app)
+    .get('/users')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body).toInclude({
+        name: 'Herman Sutanto',
+        age: 39
+      });
+    })
+    .end(done);
+})
